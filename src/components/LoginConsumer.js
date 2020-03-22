5709e91b1999bac6e0b3f057f9d57a543de9d0ac
jungbum <jungbumwoo@naver.com>
2020-03-22 22:42:54 +0900

    0322 22:42

--- src/components/LoginConsumer.js
+++ src/components/LoginConsumer.js
@@ -1,18 +1,14 @@
 import React from "react";
 import { render } from "@testing-library/react";
 
-const LoginConsumer = (props) => {
-    // 여기서 컨슈머를 받아온걸
-    // 로그인처리하고
-    // 그 담에 로그인으로 넘겨주자
-
-    render() {
+class LoginConsumer extends React.Component {
+    
+    
+    render(){
         return(
-            <>
-                <div>
-                    
-                </div>
-            </>
+            <div>
+                jungbum
+            </div>
         )
     }
 }
